ce65437e47a16eed1752d14e5d8c68069c5a3513
Allie Robinson <allierob93@gmail.com>
2019-08-11 18:17:57 -0700

    Event listeners added to the nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+navLink = document.querySelectorAll('a');
+
+navLink[0].addEventListener('click', function(){
+    document.querySelector('a').style.color = 'orange'
+})
+navLink[1].addEventListener('click', function(){
+    document.querySelectorAll('a')[1].style.color = 'red'
+})
+navLink[2].addEventListener('click', function(){
+    document.querySelectorAll('a')[2].style.color = 'skyblue'
+})
+navLink[3].addEventListener('click', function(){
+    document.querySelectorAll('a')[3].style.color = 'green'
+})
